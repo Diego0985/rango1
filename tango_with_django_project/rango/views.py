9a37be09c3d82659846a9e4b20e62051d4aac334
Diego Llerena <dfllr95@hotmail.com>
2017-09-18 21:29:32 -0500

    Templates and Media Files 1

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupckate!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, '../templates/rango/index.html', context=context_dict)
+
+    # return HttpResponse('<h1>Hello World!</h1><br><h2><a href=/rango/about>Jump to about</a>')
+
+def about(request):
+    return HttpResponse('Rango says here is the about page')
